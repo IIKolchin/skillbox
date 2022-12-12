c3bf6ddbc1ca8949b3df48d1a5ad446dbb2c30c0
Apple <IIKolchin@yandex.ru>
2022-12-12 19:09:53 +0300

    attribute key setting

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
 <template>
   <li class="catalog__item">
     <a class="catalog__pic" href="#">
@@ -12,26 +13,35 @@
 
     <ul class="colors colors--black">
       <li class="colors__item">
-        <label class="colors__label" for="color-1">
+        <label class="colors__label">
           <input
             class="colors__radio sr-only"
             type="radio"
-            name="color-1"
             value="#73B6EA"
-            checked=""
+            v-model="color"
           />
           <span class="colors__value" style="background-color: #73b6ea"> </span>
         </label>
       </li>
       <li class="colors__item">
-        <label class="colors__label" for="color-1">
-          <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000" />
+        <label class="colors__label">
+          <input
+            class="colors__radio sr-only"
+            type="radio"
+            value="#8BE000"
+            v-model="color"
+          />
           <span class="colors__value" style="background-color: #8be000"> </span>
         </label>
       </li>
       <li class="colors__item">
-        <label class="colors__label" for="color-1">
-          <input class="colors__radio sr-only" type="radio" name="color-1" value="#222" />
+        <label class="colors__label">
+          <input
+            class="colors__radio sr-only"
+            type="radio"
+            value="#222"
+            v-model="color"
+          />
           <span class="colors__value" style="background-color: #222"> </span>
         </label>
       </li>
@@ -41,6 +51,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      color: '#73B6EA',
+    };
+  },
   props: ['product'],
 };
 </script>
--- src/data/products.js
+++ src/data/products.js
@@ -1,57 +1,68 @@
 export default [
   {
+    id: 1,
     title: 'Телевизор Samsung QE75QN900AU',
     price: 515690,
     image: './img/tv.jpg',
   },
   {
+    id: 2,
     title: 'Зубная щётка Xiaomi',
     price: 5690,
     image: './img/toothbrush.jpg',
   },
   {
+    id: 3,
     title: 'Смартфон Xiaomi Mi 12Pro',
     price: 66690,
     image: './img/phone.jpg',
   },
 
   {
+    id: 4,
     title: 'Электроскейт teamgee H20T ',
     price: 38690,
     image: './img/board.jpg',
   },
   {
+    id: 5,
     title: 'Смартфон iphone 14 pro max',
     price: 145690,
     image: './img/phone-2.jpg',
   },
   {
+    id: 6,
     title: 'Электросамокат Ninebot KickScooter',
     price: 43690,
     image: './img/bicycle.jpg',
   },
 
   {
+    id: 7,
     title: 'Гироскутер MINIPRO 10.5',
     price: 12690,
     image: './img/wheels.jpg',
   },
   {
+    id: 8,
     title: 'Ноутбук Huawei MateBook X Pro',
     price: 117690,
     image: './img/notebook.jpg',
   },
   {
+    id: 9,
     title: 'Умный чайник Xiaomi Viomi Smart Kettle',
     price: 3990,
     image: './img/teapot.jpg',
   },
   {
+    id: 10,
     title: 'Умные весы Xiaomi Mi Smart Scale 2',
     price: 2290,
     image: './img/scales.jpg',
   },
   {
+    id: 11,
     title: 'Беспроводные наушники Samsung Galaxy Buds2 Pro',
     price: 12690,
     image: './img/headphones.jpg',
