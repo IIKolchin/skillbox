92a1f269a6f543a7d56ccf65a066c3d18583c6e2
Apple <IIKolchin@yandex.ru>
2023-02-13 21:43:12 +0300

    add cart-page, vuex, store

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -3,12 +3,12 @@
     <div class="content__top">
       <ul class="breadcrumbs">
         <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link" href="index.html"
-          @click.prevent="gotoPage('main')"> Каталог </a>
+          <router-link class="breadcrumbs__link" :to="{ name: 'main' }"> Каталог </router-link>
         </li>
         <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link" href="#"
-          @click.prevent="gotoPage('main')" > {{ category.title }} </a>
+          <router-link class="breadcrumbs__link" :to="{ name: 'main' }">
+            {{ category.title }}
+          </router-link>
         </li>
         <li class="breadcrumbs__item">
           <a class="breadcrumbs__link"> {{ product.title }} </a>
@@ -19,12 +19,7 @@
     <section class="item">
       <div class="item__pics pics">
         <div class="pics__wrapper">
-          <img
-            width="570"
-            height="570"
-            :src="product.image"
-            :alt="product.title"
-          />
+          <img width="570" height="570" :src="product.image" :alt="product.title" />
         </div>
       </div>
 
@@ -32,7 +27,7 @@
         <span class="item__code">{{ product.id }}</span>
         <h2 class="item__title">{{ product.title }}</h2>
         <div class="item__form">
-          <form class="form" action="#" method="POST">
+          <form class="form" action="#" method="POST" @submit.prevent="addToCart">
             <b class="item__price"> {{ product.price | numberFormat }} ₽ </b>
 
             <fieldset class="form__block">
@@ -107,16 +102,16 @@
 
             <div class="item__row">
               <div class="form__counter">
-                <button type="button" aria-label="Убрать один товар">
+                <button type="button" aria-label="Убрать один товар" @click='productAmount--'>
                   <svg width="12" height="12" fill="currentColor">
                     <use xlink:href="#icon-minus"></use>
                   </svg>
                 </button>
 
                 <label for="count">
-                  <input type="text" value="1" name="count" />
+                  <input type="text" v-model.number="productAmount" />
                 </label>
-                <button type="button" aria-label="Добавить один товар">
+                <button type="button" aria-label="Добавить один товар" @click='productAmount++'>
                   <svg width="12" height="12" fill="currentColor">
                     <use xlink:href="#icon-plus"></use>
                   </svg>
@@ -193,13 +188,18 @@ import gotoPage from '../helpers/gotoPage';
 import numberFormat from '../helpers/numberFormat';
 
 export default {
-  props: ['pageParams'],
+  // props: ['pageParams'],
+  data() {
+    return {
+      productAmount: 1,
+    };
+  },
   filters: {
     numberFormat,
   },
   computed: {
     product() {
-      return products.find((product) => product.id === this.pageParams.id);
+      return products.find((product) => product.id === +this.$route.params.id);
     },
     category() {
       return categories.find((category) => category.id === this.product.categoryId);
@@ -207,6 +207,12 @@ export default {
   },
   methods: {
     gotoPage,
+    addToCart() {
+      this.$store.commit('addProductToCart', {
+        productId: this.product.id,
+        amount: this.productAmount,
+      });
+    },
   },
 };
 </script>
